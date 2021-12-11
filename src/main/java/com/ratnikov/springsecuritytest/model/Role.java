fed56d2527d7b0000fb4d8ad3a7358f5527ad5a9
titanmet <titanmet@mail.ru>
2021-12-11 12:39:42 +0300

    step 4 authorize, Role

---
+++ src/main/java/com/ratnikov/springsecuritytest/model/Role.java
@@ -0,0 +1,6 @@
+package com.ratnikov.springsecuritytest.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/ratnikov/springsecuritytest/rest/DeveloperRestControllerV1.java
+++ src/main/java/com/ratnikov/springsecuritytest/rest/DeveloperRestControllerV1.java
@@ -1,10 +1,7 @@
 package com.ratnikov.springsecuritytest.rest;
 
 import com.ratnikov.springsecuritytest.model.Developer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,6 +26,17 @@ public class DeveloperRestControllerV1 {
         return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @PostMapping
+    public Developer create(@RequestBody Developer developer) {
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
     }
 
 }
